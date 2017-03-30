e30cec138799faec2f9bba2b583907056a6a5a77
gewl <gilbertwlawson@gmail.com>
2017-03-29 20:07:24 -0400

    Extremely rudimentary movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    GameObject player;
+    Vector3 playerPosition;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerPosition = player.transform.position;
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerPosition.x += 0.1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            playerPosition.x -= 0.1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerPosition.y += 2f;
+        }
+
+        player.transform.position = playerPosition;
+	}
+}
