e12684e484330a7005bd8e4ded31e1f7d742f162
gewl <gilbertwlawson@gmail.com>
2017-04-04 18:29:39 -0400

    Bouncing off enemy's heads, destroying them

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,4 +11,15 @@ public class EnemyController : MonoBehaviour {
 	void Update () {
         transform.Translate(new Vector3(-1.5f * Time.deltaTime, 0, 0));
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // TODO: Implement enemy dying animation
+            Destroy(gameObject);
+            // Boing!
+            collision.gameObject.GetComponent<PlayerController>().Jump();
+        }
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -50,7 +50,7 @@ public class PlayerController : MonoBehaviour {
         // Rough jump code.
         if (Input.GetKey(KeyCode.W) && vel.y == 0f)
         {
-            rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+            Jump();
         }
         else if (currentGroundColliders == 0)
         {
@@ -62,83 +62,44 @@ public class PlayerController : MonoBehaviour {
         playerAnim.SetFloat("Speed", absoluteXVelocity);
     }
 
-    // Function to take 'current' horizontal speed and update based on player input.
-    // CURRENTLY UNUSED: Reverted to "InputAxis" method.Just keeping for a commit or two in case of reversion.
-    //float displacementUpdater(float speed)
-    //{
-    //    // Max speed of 0.2f in a given direction.
-    //    if (Input.GetKey(KeyCode.D) && speed <= 0.2f)
-    //    {
-    //        // This code handles acceleration — fast at first, slower near upper ranges.
-    //        float absSpeed = Mathf.Abs(speed);
-    //        if (absSpeed >= 0.0f && absSpeed < 0.1f)
-    //        {
-    //            speed += 0.05f;
-    //        }
-    //        else if (absSpeed >= 0.1f && absSpeed < .15f)
-    //        {
-    //            speed += 0.02f;
-    //        }
-    //        else if (absSpeed >= .15f && absSpeed < 0.2f)
-    //        {
-    //            speed += 0.01f;
-    //        }
-    //        else if (speed == -0.2f)
-    //        {
-    //            speed += 0.01f;
-    //        }
-    //    }
-    //    else if (Input.GetKey(KeyCode.A) && speed >= -0.2f)
-    //    {
-    //        float absSpeed = Mathf.Abs(speed);
-    //        if (absSpeed >= 0.0f && absSpeed < 0.1f)
-    //        {
-    //            speed -= 0.05f;
-    //        }
-    //        else if (absSpeed >= 0.1f && absSpeed < .15f)
-    //        {
-    //            speed -= 0.02f;
-    //        }
-    //        else if (absSpeed >= .15f && absSpeed < 0.2f)
-    //        {
-    //            speed -= 0.01f;
-    //        }
-    //        else if (speed == 0.2f)
-    //        {
-    //            speed -= 0.01f;
-    //        }
-    //    }
-    //    // Speed decays quickly if player's aren't supplying input.
-    //    // Feels much better than just killing speed in absence of input.
-    //    else if (!Input.GetKey(KeyCode.D) && speed > 0.0f)
-    //    {
-    //        speed -= 0.05f;
-    //    }
-    //    else if (!Input.GetKey(KeyCode.A) && speed < 0.0f)
-    //    {
-    //        speed += 0.05f;
-    //    }
+    // Getting ready for separating rendering & logic
+    // Public so enemy logic can call on head-hit
+    public void Jump()
+    { 
+        // Used for jumping off enemy's heads—downward momentum was
+        // nullifying the bounce. This way there's a satisfying "boing"
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+        rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+    }
 
-    //    //TODO: Speed values weren't staying rounded to two-decimal places, I'm not sure why.
-    //    // This is an ungainly solution in the meantime — forces them to re-round at the end of each frame.
-    //    speed = Mathf.Round(speed * 100f) / 100f;
-
-    //    // Return for reassignation in update function.
-    //    return speed;
-    //}
-
-    //CURRENTLY UNUSED: Switched to "velocity.y == 0" test for groundedness.Keeping code in case of reversion.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == groundLayerId)
+        GameObject collisionGo = collision.gameObject;
+
+        switch (collisionGo.tag)
         {
-            currentGroundColliders++;
+            case "Ground":
+                currentGroundColliders++;
+                break;
+            // TODO: Implement damage
+            // This refers to the "body" of the enemy, not the boingable head.
+            case "Enemy":
+                Debug.Log("ouch!");
+                break;
+            default:
+                Debug.Log(collisionGo.tag);
+                break;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == groundLayerId)
+    { 
+        GameObject collisionGo = collision.gameObject;
+
+        if (collisionGo.layer == groundLayerId)
         {
             currentGroundColliders--;
         }
