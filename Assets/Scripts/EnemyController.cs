484ecf116d30fcc6b3504068de50671611a7570b
gewl <gilbertwlawson@gmail.com>
2017-04-06 16:17:44 -0400

    Death animation for enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
+    public BoxCollider2D bodyCollider;
+    public BoxCollider2D headCollider;
+
     private ScoreController scoreController;
+    private Rigidbody2D rb;
+
+    private bool isDead = false;
 
 	void Start () {
         scoreController = GameObject.Find("ScoreDisplay").GetComponent<ScoreController>();
+
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
-	void Update () {
-        transform.Translate(new Vector3(-1.5f * Time.deltaTime, 0, 0));
+	void FixedUpdate () {
+        if (!isDead)
+        {
+            transform.Translate(new Vector3(-1.5f * Time.deltaTime, 0, 0));
+        } else
+        {
+            Vector2 vel = rb.velocity;
+            vel.y -= 50f * Time.deltaTime;
+            rb.velocity = vel;
+        }
+        
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            isDead = true;
+
+            rb.AddForce(transform.up * 500f);
+
+            GetComponent<SpriteRenderer>().flipY = true;
+
             // TODO: Implement enemy dying animation
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            bodyCollider.enabled = false;
+            headCollider.enabled = false;
+
             // Boing!
             collision.gameObject.GetComponent<PlayerController>().Jump();
             // Add to/update score.
