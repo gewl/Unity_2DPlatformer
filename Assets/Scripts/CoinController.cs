52e067bcadbcb455a9022e0d6a8fb04d0175ffef
gewl <gilbertwlawson@gmail.com>
2017-04-05 21:00:04 -0400

    Added coins, anim/sprite for coins, valueDict in ScoreController

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -2,28 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour {
+public class CoinController : MonoBehaviour {
 
     private ScoreController scoreController;
 
-	void Start () {
+    void Start () {
         scoreController = GameObject.Find("ScoreDisplay").GetComponent<ScoreController>();
-	}
+    }
 	
 	void Update () {
-        transform.Translate(new Vector3(-1.5f * Time.deltaTime, 0, 0));
+		
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            // TODO: Implement enemy dying animation
             Destroy(gameObject);
-            // Boing!
-            collision.gameObject.GetComponent<PlayerController>().Jump();
-            // Add to/update score.
-            scoreController.increaseScore("Enemy");
+            scoreController.increaseScore("Coin");
         }
     }
 }
--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -10,25 +10,38 @@ public class ScoreController : MonoBehaviour {
     private string scoreTextPhrase = "Current score: ";
     private int currentScore;
 
+    // Dict to hold values of different score-incrementers (enemies, coins, etc.) in one centralized place for easy tweaking.
+    // Initialized by running 'initializeDict' function on start.
+    Dictionary<string, int> valueDict;
+
 	void Start () {
         scoreText = GetComponent<Text>();
 
         currentScore = 0;
 
         updateUIText();
+        initializeDict();
 	}
 	
 	void Update () {
 		
 	}
 
+    void initializeDict()
+    {
+        valueDict = new Dictionary<string, int>();
+        valueDict.Add("Coin", 100);
+        valueDict.Add("Enemy", 50);
+    }
+
     void updateUIText ()
     {
         scoreText.text = scoreTextPhrase + currentScore;
     }
 
-    public void increaseScore (int value)
+    public void increaseScore (string name)
     {
+        int value = valueDict[name];
         currentScore += value;
         updateUIText();
     }
