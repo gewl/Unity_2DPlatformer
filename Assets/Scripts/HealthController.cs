818ff0fd3eb8f34b2aef7c5f0461b6df4e3bc606
gewl <gilbertwlawson@gmail.com>
2017-04-07 17:04:14 -0400

    UI hearts added (not wired to logic yet)

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+    int currentHealth;
+
+    private GameObject heartPrefab;
+    protected GameObject[] uiHearts;
+
+    private const string HEART_PREFAB = "Prefabs/UIElements/UIHeart";
+
+	void Start () {
+
+        currentHealth = 3;
+        uiHearts = new GameObject[currentHealth];
+
+        heartPrefab = Resources.Load<GameObject>(HEART_PREFAB);
+
+        for (int i = 0; i < currentHealth; i++)
+        {
+            GameObject uiHeart = Instantiate(heartPrefab, this.transform, false);
+            uiHeart.name = "Heart_" + i;
+            uiHeart.transform.localPosition = new Vector2(15f + (i * 35f), -15f);
+
+            uiHearts[i] = uiHeart;
+        }
+
+    }
+	
+	void Update () {
+
+        
+	}
+}
