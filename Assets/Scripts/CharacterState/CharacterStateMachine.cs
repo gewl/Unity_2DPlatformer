a76c682be12a948c7d2472b0da98c5dbb3e06ab1
gewl <gilbertwlawson@gmail.com>
2017-05-20 21:45:22 -0400

    state & state machine classes implemented, ready for inheritors

--- Assets/Scripts/CharacterState/CharacterStateMachine.cs
+++ Assets/Scripts/CharacterState/CharacterStateMachine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,24 +11,108 @@ public class CharacterStateMachine : MonoBehaviour {
 
     public object Parent { get { return parent; } }
 
-    private State previousState;
-    public State PreviousState { get { return previousState; } }
+    private CharacterState previousState;
+    public CharacterState PreviousState { get { return previousState; } }
 
-    private State currentState;
-    public State CurrentState { get { return currentState; } }
+    private CharacterState currentState;
+    public CharacterState CurrentState { get { return currentState; } }
 
-    private State nextState;
-    public State NextState { get { return nextState; } }
+    private CharacterState nextState;
+    public CharacterState NextState { get { return nextState; } }
 
-    private bool force = false;
+    private bool forced = false;
     
     public CharacterStateMachine (CharacterStateMachine parent)
     {
         this.parent = parent;
     }
+
+    public CharacterStateMachine (MonoBehaviour parent)
+    {
+        this.parent = parent;
+    }
+
+    public void Update()
+    {
+        if (nextState != null)
+        {
+            if (currentState != null)
+            {
+                previousState = currentState;
+                previousState.Exit();
+            }
+
+            currentState = nextState;
+            currentState.Enter();
+
+            nextState = null;
+
+            forced = false;
+        }
+
+        if (currentState != null)
+        {
+            currentState.Update();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        currentState.FixedUpdate();
+    }
+
+    public void SwitchState (CharacterState nextState)
+    {
+        if (forced)
+        {
+            return;
+        }
+
+        if (nextState == null || (nextState != null && this.nextState != null && nextState.Priority<this.nextState.Priority))
+        {
+            return;
+        }
+
+        this.nextState = nextState;
+    }
+
+    public void ForceSwitchState (CharacterState nextState)
+    {
+        if (forced)
+        {
+            return;
+        }
+
+        this.nextState = nextState;
+        forced = true;
+    }
+
+    public bool IsInState (Type type)
+    {
+        return currentState.GetType() == type || (nextState != null && nextState.GetType() == type);
+    }
 }
 
-public abstract class State
+public abstract class CharacterState
 {
-      
+    private CharacterStateMachine machine;
+
+    public CharacterStateMachine Machine { get { return machine; } }
+
+    protected int priority = 1;
+
+    public int Priority { get { return priority; } }
+
+    public CharacterState (CharacterStateMachine machine)
+    {
+        this.machine = machine;
+    }
+
+    public virtual void Enter() { }
+
+    public virtual void Exit() { }
+
+    public virtual void Update() { }
+
+    public virtual void FixedUpdate() { }
 }
